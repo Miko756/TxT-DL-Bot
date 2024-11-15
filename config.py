ac04b566c65f3d845031ec86e0f15af339c5b87d
Yae Miko <164617880+Miko756@users.noreply.github.com>
2024-11-15 05:57:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import re, os, time
 import datetime
 
 class Config:
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
-    API_HASH = os.environ.get('API_HASH')
-    API_ID = int(os.environ.get('API_ID'))
-    OWNER_ID = int(os.environ.get('OWNER_ID')) #Admin UserId
-    DB_URI = os.environ.get('DB_URI') #May Be In Future.
+    BOT_TOKEN = os.environ.get('7716433955:AAFyRm41MWU-K0MslxcRWvE5tOYFMCS3pO8')
+    API_HASH = os.environ.get('25278a8394b5914ee1b8d6a6c79d572d')
+    API_ID = int(os.environ.get('27692015'))
+    OWNER_ID = int(os.environ.get('7092511418')) #Admin UserId
+    DB_URI = os.environ.get('mongodb+srv://Premium:aloksingh@cluster0.4ykpo.mongodb.net/?retryWrites=true&w=majority') #May Be In Future.
     
