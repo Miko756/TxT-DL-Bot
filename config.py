dd9a0919a6481e3072228fd04d7d8b6f3229deb1
Yae Miko <164617880+Miko756@users.noreply.github.com>
2024-11-15 06:00:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import datetime
 class Config:
     BOT_TOKEN = os.environ.get('7716433955:AAFyRm41MWU-K0MslxcRWvE5tOYFMCS3pO8')
     API_HASH = os.environ.get('25278a8394b5914ee1b8d6a6c79d572d')
-    API_ID = int(os.environ.get('27692015'))
+    API_ID = int(os.environ.get(API_ID, '27692015'))
     OWNER_ID = int(os.environ.get('7092511418')) #Admin UserId
     DB_URI = os.environ.get('mongodb+srv://Premium:aloksingh@cluster0.4ykpo.mongodb.net/?retryWrites=true&w=majority') #May Be In Future.
     
